800425ead810e3489bb7bbe0ff47a04d8903e850
yoGothwal <167828920+yoGothwal@users.noreply.github.com>
2024-11-08 01:34:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express')
+
 const app = express();
-const PORT = 3001;
+
+const PORT = process.env.NODE_ENV === 'production' ? 3001 : 5000; // Change 5000 to your desired development port
+
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
 const cors = require('cors')
 app.use(cors())
 app.use(express.json())
